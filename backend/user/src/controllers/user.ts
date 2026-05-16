e59d2970ecd017f61815e503f6a6da721479de0b
Taha Sayyed <codetaha786@gmail.com>
2026-05-16 20:17:47 +0530

    feat(profile): add user profile retrieval and update functionality

--- backend/user/src/controllers/user.ts
+++ backend/user/src/controllers/user.ts
@@ -3,7 +3,7 @@ import TryCatch from "../config/TryCatch.js"
 import { redisClient } from "../index.js";
 import { User } from "../model/User.js";
 import { generateToken } from "../config/generateToken.js"
-
+import { AuthenticatedRequest } from "../middleware/isAuth.js"
 
 export const loginUser = TryCatch(async (req, res) => {
     const { email } = req.body
@@ -81,3 +81,41 @@ export const verifyUser = TryCatch(async (req, res) => {
     });
 })
 
+export const myProfile = TryCatch(async (req: AuthenticatedRequest, res) => {
+    const user = req.user;
+    res.json(user);
+})
+
+export const updateName = TryCatch(async (req: AuthenticatedRequest, res) => {
+  const user = await User.findById(req.user?._id);
+
+  if (!user) {
+    res.status(404).json({
+      message: "Please login",
+    });
+    return;
+  }
+
+  user.name = req.body.name;
+
+  await user.save();
+
+  const token = generateToken(user);
+
+  res.json({
+    message: "User Updated",
+    user,
+    token,
+  });
+});
+
+export const getAllUsers = TryCatch(async (req: AuthenticatedRequest, res) => {
+  const users = await User.find();
+
+  res.json(users);
+});
+
+export const getAUser = TryCatch(async (req, res) => {
+  const user = await User.findById(req.params.id);
+  res.json(user);
+});
