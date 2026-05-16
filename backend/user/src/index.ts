2ced163bd182cf99095702f22cf9f55765b3437d
Taha Sayyed <codetaha786@gmail.com>
2026-05-16 11:56:58 +0530

    feat(redis): add Redis client setup and connection handling

--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./config/db.js";
 import dns from "node:dns/promises";
+import { createClient } from "redis";
 
 //👇For Development only
 dns.setServers(["1.1.1.1"]);
@@ -9,6 +10,15 @@ dotenv.config();
 //👆For Development only
 
 connectDb();
+
+export const redisClient = createClient({
+  url: process.env.REDIS_URL,
+});
+
+redisClient
+  .connect()
+  .then(() => console.log("✅connected to redis"))
+  .catch(console.error);
 
 const app = express();
 
